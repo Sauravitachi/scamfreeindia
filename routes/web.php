72e4412daa8238198fa62422cf162783c87c2f8b
saurav itachi <sauravkumar52778@gmail.com>
2026-01-05 13:55:54 +0530

    done

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Public route to serve status-files from storage
+Route::get('/storage/uploads/status-files/{filename}', function ($filename) {
+    $path = storage_path('app/public/uploads/status-files/' . $filename);
+    if (!file_exists($path)) {
+        abort(404);
+    }
+    $mimeType = mime_content_type($path);
+    return response()->file($path, [
+        'Content-Type' => $mimeType,
+        'Cache-Control' => 'public, max-age=86400',
+    ]);
+});
